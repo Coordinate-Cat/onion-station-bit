2ac3f3abef6d0279338d67603d7b84ac25fd2806
Stackbit <projects@stackbit.com>
2020-05-11 12:11:09 +0000

    initial commit by Stackbit

---
+++ src/components/Layout.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Helmet} from 'react-helmet';
+import _ from 'lodash';
+
+import {safePrefix} from '../utils';
+import Header from './Header';
+import Footer from './Footer';
+
+export default class Body extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Helmet>
+                    <title>{_.get(this.props, 'pageContext.frontmatter.title') && _.get(this.props, 'pageContext.frontmatter.title') + ' - '}{_.get(this.props, 'pageContext.site.siteMetadata.title')}</title>
+                    <meta charSet="utf-8"/>
+                    <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
+                    <meta name="viewport" content="width=device-width, initialScale=1, userScalable=no" />
+                    <meta name="google" content="notranslate" />
+                    {(_.get(this.props, 'pageContext.frontmatter.template') === 'post') &&  
+                    _.get(this.props, 'pageContext.frontmatter.canonical_url') && 
+                    <link rel="canonical" href={_.get(this.props, 'pageContext.frontmatter.canonical_url')}/>
+                    }
+                </Helmet>
+                <div className="wrapper">
+                    <Header {...this.props} />
+                    <div className="container">
+                        {this.props.children}
+                    </div>
+                    <Footer {...this.props} />
+                </div>
+            </React.Fragment>
+        );
+    }
+}
---
+++ src/pages/index.md
@@ -0,0 +1,27 @@
+---
+title: Home
+sections:
+  - section_id: hero
+    component: HeroBlock
+    type: heroblock
+    title: I love movie & cat
+    image: >-
+      https://res.cloudinary.com/practicaldev/image/fetch/s--uDUukuLl--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/380094/d5a41c85-c807-4c1b-966d-c6ac03890d6d.png
+  - title: Text
+    section_id: intro
+    component: ContentBlock
+    type: contentblock
+  - title: Recent Posts
+    section_id: posts
+    component: Postlist
+    type: postlist
+  - title: A Cat
+    section_id: cat
+    type: textimageblock
+    component: TextImageBlock
+menus:
+  main:
+    title: Home
+    weight: 1
+template: home
+---
